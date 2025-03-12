f4080033f156e90effab3c42c50a87a4c21fcb24
vintendohuynh <vintendohuynh@gmail.com>
2025-03-12 13:56:05 -0700

    Frontend start
    signin/signup and create event buttons route to templated

--- app.py
+++ app.py
@@ -13,7 +13,15 @@ def index():
     except Exception as e:
         app.logger.error("Error fetching messages: %s", e)
         return "An error occurred while fetching data.", 500
-    return render_template('test2.html', data=data)
+    return render_template('testhome.html', data=data)
+
+@app.route('/signin')
+def signin():
+    return render_template('signin.html')
+
+@app.route('/createEvent')
+def create_event():
+    return render_template('createEvent.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
