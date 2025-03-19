c58a56354e69625bd3d445f51da2941cc7e97119
vintendohuynh <vintendohuynh@gmail.com>
2025-03-19 13:03:37 -0700

    slideshow and create event update

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -1,5 +1,5 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex); // Start the slideshow
 
 // Next/previous controls
 function plusSlides(n) {
@@ -15,14 +15,22 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+
+  if (n > slides.length) {
+    slideIndex = 1; // Loop back to first slide
   }
+  if (n < 1) {
+    slideIndex = slides.length; // Loop to last slide
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; // Hide all slides
+  }
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+
+  slides[slideIndex - 1].style.display = "block"; // Show the current slide
+  dots[slideIndex - 1].className += " active";
 }
