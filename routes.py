7c6e2635c0e183368386323ae3d79f4c09a5e67b
Carlos Anguiano <carlosanguiano137@gmail.com>
2025-04-09 23:08:44 -0700

    add sample form for posting new messages and committing to db

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 #defines the routes for the application
 from flask import Blueprint, Response, render_template, request, redirect, url_for
 from models import User, Guestbook, Message
+from werkzeug.security import generate_password_hash
 from db import db
 
 #declare "routes" to be a blueprint holding all of the routes we define
@@ -20,17 +21,19 @@ def create_user():
     #only want to create user when accessed via POST request
     if request.method == "POST":
         #retrieve parameters here
-        form_details = request.form.to_dict()
-        print(form_details)
+        form_data = request.form.to_dict()
+        
+        #hash the password so it is not stored in plaintext
+        #werkzeug has a built-in hash password function! no external libs needed :)
+        form_data["password_hash"] = generate_password_hash()
 
-        #TODO hash password here
         #form_details['user-pass'] = hashpassword(form_details['user-pass'])
         event_details=f'''
             NEW USER REQUEST. CREATING USER...\n
-            first_name: {form_details['first_name']}\n
-            last_name:  {form_details['last_name']}\n
-            email:      {form_details['email']}\n
-            password:   {form_details['password_hash']}\n
+            first_name: {form_data['first_name']}\n
+            last_name:  {form_data['last_name']}\n
+            email:      {form_data['email']}\n
+            password:   {form_data['password_hash']}\n
             '''
         #call db function here? e.g.
         #create_user_in_table(firstname=XX, lastname=XX, email=XX, pass=hash(XX))
@@ -48,12 +51,11 @@ def login_user():
     #only want to create user when accessed via POST request
     if request.method == "POST":
         #retrieve parameters here
-        form_details = request.form.to_dict()
-        print(form_details)
+        form_data = request.form.to_dict()
         login_details =f'''
             USER IS LOGGING IN WITH DETAILS...\n
-            email:          {form_details['email'] if form_details['email'] else None}\n
-            password_hash (attempted) {form_details['password_hash'] if form_details['password_hash'] else None}\n
+            email:          {form_data["email"]}\n
+            password_hash (attempted) {form_data["password_hash"]}\n
             '''
     return Response(login_details, mimetype='text/plain')
 
@@ -78,6 +80,7 @@ def handle_new_event():
             event_time:         {form_details['event_time']} \n
             '''
     #TODO figure out whether we need to redirect to confirmation page or if we can redirect straight to share page
+    #I believe we'll be able to redirect straight to the share page, but need to figure this out
     #TODO replace 1 with actual event id
     return redirect('/share/1')
 
@@ -108,7 +111,6 @@ def render_event_page():
 def post_message_form():
     return render_template('testpost.html', data=dict(event_id="1"))
 #this route will be used to populate the database with incoming messages for a specific event page
-#TODO fix query for posting message to database
 #TODO will need to pass the event id when we try and post a message for now, see if we need to fix this?
 @routes.route('/postMessage/<event_id>', methods=['POST'])
 def post_message(event_id):
