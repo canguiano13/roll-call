4b492003eb795e18a8d8a6577335a819b10006a4
Carlos Anguiano <carlosanguiano137@gmail.com>
2025-04-11 16:14:20 -0700

    Add query to event page to fetch event details and messages

--- routes.py
+++ routes.py
@@ -1,18 +1,20 @@
-#defines the routes for the application
-from flask import Blueprint, Response, render_template, request, redirect, url_for
+#file defines the routes for the application
+from flask import Blueprint, Response, render_template, request, redirect, url_for, abort
 from models import User, Guestbook, Message
-from werkzeug.security import generate_password_hash, check_password_hash
 from db import db
+from sqlalchemy import desc
+from werkzeug.security import generate_password_hash, check_password_hash
 
-#declare "routes" to be a blueprint holding all of the routes we define
+#declare a blueprint hto hold all of our defined routes, expose templates folder
 routes = Blueprint("routes", __name__, template_folder="templates")
 
-#DEFINE APP ROUTES
 #TODO fix routes to be in all lowercase, probably better ux
 @routes.route('/')
 def index():
     return render_template('testhome.html')
 
+#--------------- USER/SESSION MANAGEMENT------------------------
+#TODO figure out if we can use flask login management library here
 #handles new user signups
 @routes.route('/signUp')
 def signup():
@@ -45,8 +47,6 @@ def create_user():
         
         #then route to success page?
     return Response(event_details, mimetype='text/plain')
-
-#TODO figure out if we can use flask login management library here
 #Handles user logins
 @routes.route('/signIn')
 def signin():
@@ -72,53 +72,85 @@ def login_user():
             '''
     return Response(login_details, mimetype='text/plain')
 
-
-#creating and handling new event
+#----------------EVENT/MESSAGE MANAGEMENT------------------------
+#render template for new event
 @routes.route('/createEvent')
 def create_event():#
     return render_template('createEvent.html')
+
+#TODO fix method
 #For handling request form data we can get the form inputs value by using POST attribute.
 @routes.route('/handleCreateEvent', methods=["POST"]) 
 def handle_new_event():
     if request.method == "POST":
         #retrieve parameters here
-        form_details = request.form.to_dict()
-        print(form_details)
-        #form into datetime
+        form_data = request.form.to_dict()
+        print(form_data)
         event_details=f'''
             NEW EVENT REQUEST... \n
-            event_title:        {form_details['event_title']}\n
-            event_description:  {form_details['event_address']}\n
-            event_datetime:         {form_details['event_date']} \n 
-            event_time:         {form_details['event_time']} \n
+            event_title:        {form_data['event_title']}\n
+            event_description:  {form_data['event_address']}\n
+            event_datetime:     {form_data['event_date']} \n 
+            event_time:         {form_data['event_time']} \n
             '''
-    #TODO figure out whether we need to redirect to confirmation page or if we can redirect straight to share page
-    #I believe we'll be able to redirect straight to the share page, but need to figure this out
-    #TODO replace 1 with actual event id
-    return redirect('/share/1')
+    
+        #TODO Transform separate data/time entries into DATETIME data type
+        #event_datetime=...
 
+        #TODO need to figure out how we are sourcing the owner_id
+        #TODO figure out how to handle the images, they are optional so some guestbooks will have them and others will not
+        #using the form details and user's account data, create a new guestbook associated with the user
+        #new_guestbook = Guestbook(owner_id=TODO, event_date=event_datetime, event_title=form_data['event_title'], 
+        #                          event_address=form_data['event_address'], event_img=TODO)
+
+        #add and commit the new guestbook to our database
+        #db.session.add(new_guestbook)
+        #db.session.commit()
+
+        #return redirect(f'/share/{new_guestbook.event_id}')
+        return redirect('/share/1')
 
 #render a custom share page for the event
 @routes.route('/share/<event_id>')
 def share_event(event_id):
-    #first query the name, address, date of the event using passed event id
+    #first query the event details
     event_data = db.session.query(Guestbook).get(event_id)
     #if the event doesn't exist, throw a 404 error
     if event_data is None:
-        return render_template("404.html"), 404
-    print(event_data)
+        abort(404)
     return render_template('shareEvent.html', data=event_data)
 
+#TODO define html template for event page
+@routes.route('/event/<event_id>', methods=['GET'])
+def render_event_page(event_id):
+    #get all of the event details based on event id
+    event_data = db.session.query(Guestbook).get(event_id)
+    #get the list of messages for the event, then put them in order from most to least recent
+    messages_data= db.session.query(Message).filter(Message.event_id==event_id).order_by(desc(Message.msg_id)).all()
+    #if the event doesn't exist, redirect to a 404
+    if event_data is None:
+        abort(404)
+    #TODO define action when an event exists but there are no messages                     
+    elif messages_data is None:
+        print("no messages.")
+    return render_template('event.html', event_data=event_data, messages_data=messages_data)
 
-#TODO define skeleton for event page
-# @routes.route('/event/{event-id}', methods=['GET'])
-# def render_event_page(event_id):
-#     data = db.session.query(Message).filter(Message.event_id == {event_id})
-#     return render_template('event.html', data=data)
-@routes.route('/event')
-def render_event_page():
-    return render_template('event.html')
+#this route will be used to populate the database with incoming messages for a specific event page
+#TODO will need to pass the event id when we try and post a message for now, see if we need to fix this?
+@routes.route('/handlePostMessage/<event_id>', methods=['POST'])
+def post_message(event_id):
+    #TODO if for some reason, we can't get certain form data, redirect to a failure page or put an alert on screen or something
+    form_data = request.form.to_dict()
+    #using the form data, create a new message object. this works because we have the ORM mapping
+    new_message = Message(event_id=event_id, display_name=form_data["display_name"], message_content=form_data["message_content"])
+    #add the new message to our database
+    db.session.add(new_message)
+    #commit it to the database
+    db.session.commit()
+    #TODO fix to redirect to event page, redirect back to share page for now
+    return redirect(f'/allmessages')
 
+#---------------PREDEFINED TEMPLATES------------------------
 #birthday template can route here
 #TODO create template and refine redirect logic
 @routes.route('/birthdayTemplate')
@@ -140,28 +172,16 @@ def halloween_template():
 def st_paddy_template():
     return "happy st. paddy's day"
 
-#TODO route new messages to this method. Method will take the form data and push them to the page
-@routes.route('/testpostmessage')
-def post_message_form():
-    return render_template('testpost.html', data=dict(event_id="1"))
-#this route will be used to populate the database with incoming messages for a specific event page
-#TODO will need to pass the event id when we try and post a message for now, see if we need to fix this?
-@routes.route('/postMessage/<event_id>', methods=['POST'])
-def post_message(event_id):
-    #TODO if for some reason, we can't get certain form data, redirect to a failure page or put an alert on screen or something
-    form_data = request.form.to_dict()
-    #using the form data, create a new message object. this is fine because we have the ORM mapping
-    new_message = Message(event_id=event_id,
-                           display_name=form_data["display_name"],
-                           message_content=form_data["message_content"])
-    #add the new message to our database
-    db.session.add(new_message)
-    #commit it to the database
-    db.session.commit()
-    #TODO fix to redirect to event page, redirect back to share page for now
-    return redirect(f'/allmessages')
+#--------------- ERROR HANDLING ------------------------
+#404: page not found
+@routes.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
+#---------------#TODO can remove these later------------------------
 #routes that execute some simple database queries to fetch data
+#uses the model ORM to fetch all the messages from the database
+#equivalent to "SELECT * FROM" statements
 @routes.route('/allusers', methods=["GET"])
 def getallusers():
     users = db.session.query(User).all()
@@ -174,8 +194,6 @@ def getallguestbooks():
     return Response(guestbook_list, mimetype="text/plain")
 @routes.route('/allmessages', methods=["GET"])
 def getallmessages():
-    #uses the model ORM to fetch all the messages from the database
-    #equivalent to SELECT * FROM messages
     messages = db.session.query(Message).all()
     message_list = "\n".join(f"{msg.display_name} says: {msg.message_content}" for msg in messages)
     return Response(message_list, mimetype="text/plain")
