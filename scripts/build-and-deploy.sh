d71bba77aef4141ef162e1c8da4140eaac32645e
Carlos Anguiano <carlosanguiano137@gmail.com>
2025-04-30 00:30:40 -0700

    update unfinished comments for build-and-deploy script

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 #get root folder
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PROJECT_ROOT="$SCRIPT_DIR/.."
@@ -9,7 +10,7 @@ export PROJECT_ID=halogen-sol-452703-b5
 export REGION=us-west1
 export CONNECTION_NAME=halogen-sol-452703-b5:us-west2:guestbook-db
 
-#build the container and add it onto
+#build the container using Dockerfile in root folder and deploy it to Google Cloud Run
 gcloud builds submit "$PROJECT_ROOT" \
     --tag gcr.io/$PROJECT_ID/guestbook-app \
     --project $PROJECT_ID
